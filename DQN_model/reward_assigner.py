9e24488916c2aa9f4851dce026395de50f726fad
John Rader <jayhawkjohnr@gmail.com>
2024-03-23 20:57:09 -0500

    Create reward_assigner.py

---
+++ DQN_model/reward_assigner.py
@@ -0,0 +1,11 @@
+class RewardAssigner:
+
+    def assign_reward(past_logic, present_logic):
+        pass
+    
+        # Plan 1:
+        # A small reward should be given whenever a number is combined
+        # A medium reward should be given when a 100 is reached
+
+        # For the following: the "bottom" should be defined as the lowest row that is above a number of complete 
+        # When a new 100 is reached, grant points based on how close it is to the bottom
